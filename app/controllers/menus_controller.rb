131a1cd5e47b85c7d9b31d5d22054433c28074b9
s1211431@u.tsukuba.ac.jp <s1211431@u.tsukuba.ac.jp>
2016-01-27 22:13:01 +0900

    menu settings

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -15,10 +15,24 @@ class MenusController < ApplicationController
   # GET /menus/new
   def new
     @menu = Menu.new
+    menu_arrivals = Arrival.all.distinct.pluck(:id,:bean_id)
+    @beans_suppliers = Array.new
+    menu_arrivals.each do |arrival_id, bean_id|
+      if bean = Bean.find(bean_id) then
+        @beans_suppliers << ["#{bean[:name]}/#{bean.supplier[:name]}",arrival_id]
+      end
+    end
   end
 
   # GET /menus/1/edit
   def edit
+    menu_arrivals = Arrival.all.distinct.pluck(:id,:bean_id)
+    @beans_suppliers = Array.new
+    menu_arrivals.each do |arrival_id, bean_id|
+      if bean = Bean.find(bean_id) then
+        @beans_suppliers << ["#{bean[:name]}/#{bean.supplier[:name]}",arrival_id]
+      end
+    end
   end
 
   # POST /menus
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,17 @@
 class Menu < ActiveRecord::Base
-  belongs_to :arrival, foreign_key "arrival_id"
-  has_many :orders
+  belongs_to :arrival, foreign_key: "arrival_id"
+  has_many :orders, foreign_key: "menu_id"
+  
+  validates :arrival_id,
+            presence: true,
+            inclusion: {in: Arrival.pluck(:id)}
+  validates :price,
+            presence: true,
+            numericality: {greater_than_or_equal_to: 0}
+  validates :temperature,
+            presence: true,
+            inclusion: {in: ['hot','iced']}
+  validates :variety,
+            presence: true,
+            inclusion: {in: ['black','cafe_au_lait',]}
 end
