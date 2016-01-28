92c7f15c22649650098ccbf8f3c1bb9cc8177fe2
s1211431@u.tsukuba.ac.jp <s1211431@u.tsukuba.ac.jp>
2016-01-29 05:51:22 +0900

    navbar setting

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,10 @@
 class Order < ActiveRecord::Base
-  belongs_to :menu, foreign_key "menu_id"
+  belongs_to :menu, foreign_key: "menu_id"
   
-  validation :menu_id,
+  validates :menu_id,
              presence: true,
              inclusion: {in: Menu.pluck(:id)}
-  validation :person_id,
+  validates :person_id,
              presence: true,
              inclusion: {in: Person.pluck(:id)}
 end
--- app/views/arrivals/index.json.jbuilder
+++ app/views/arrivals/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@arrivals) do |arrival|
-  json.extract! arrival, :id, :bean_id, :arrival_type, :arrival_date, :soldout, :soldout_date
+  json.extract! arrival, :id, :bean_id, :arrival_type, :amount, :arrival_date, :soldout, :soldout_date
   json.url arrival_url(arrival, format: :json)
 end
